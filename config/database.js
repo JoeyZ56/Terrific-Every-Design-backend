ecb96a4fefed60863c3d5e1637163586f7d07503
username <Jzazzi56@gmail.com>
2024-07-18 13:05:48 -0700

    added database connection

---
+++ config/database.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const conncectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+
+    console.log("Connected to Mongo Database");
+  } catch (error) {
+    console.error("Error connecting to Mongo Database: ", error);
+    process.exit(1); // Exit process with failure
+  }
+};
+
+module.exports = conncectDB;
